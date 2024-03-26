2a67efad037e08ce51717dbd63102d91d92c6de9
Eric Chan <eric.chan@accelbyte.net>
2024-03-26 16:38:32 -0700

    fix: patch tag-module script for new modules to be released seamlessly

--- scripts/tag-module.sh
+++ scripts/tag-module.sh
@@ -8,11 +8,21 @@ test -n "$MODULE_NAME" || (echo "MODULE_NAME is not set"; exit 1)
 
 MODULE_NAME="module-$MODULE_NAME"
 LAST_TAG=$(git tag --list "$MODULE_NAME/*" --sort -version:refname | head -n 1)
-LAST_COMMIT=$(git log --format="%H" -n 1)
+echo -e "Checking Git tag --list for $MODULE_NAME: $LAST_TAG"
 
-CHANGE_COUNT=$(git diff --name-only "$LAST_TAG" | { grep -c "$MODULE_NAME/version.txt" || test $? = 1; } | { grep -v grep || test $? = 1; })
+LAST_COMMIT=$(git log --format="%H" -n 1)
+echo -e "- last commit: $LAST_COMMIT"
+
+if [ -z "$LAST_TAG" ]; then
+    echo -e "*** This module: $MODULE_NAME has no existing tags - meaning it's NEW! ***"
+    CHANGE_COUNT=1
+else
+    CHANGE_COUNT=$(git diff --name-only "$LAST_TAG" | { grep -c "$MODULE_NAME/version.txt" || test $? = 1; } | { grep -v grep || test $? = 1; })
+    echo -e "- change count: $CHANGE_COUNT"
+fi
+
 if [ "$CHANGE_COUNT" -gt 0 ]; then
-    echo "Tagging $MODULE_NAME..."
+    echo -e ">>> Tagging $MODULE_NAME..."
 
     VERSION_INFO=$(cat "$MODULE_NAME/version.txt" | tail -n 1)
     echo -e "\t New version: $VERSION_INFO"
