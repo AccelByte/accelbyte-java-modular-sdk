a00662c788735fb3e3fb4ff81424123068604396
ruli sastra putri <ruli@accelbyte.net>
2024-12-25 22:29:32 +0700

    chore(test): integration - use correct username

--- all-module/src/test/java/net/accelbyte/sdk/integration/TestIntegrationServiceIam.java
+++ all-module/src/test/java/net/accelbyte/sdk/integration/TestIntegrationServiceIam.java
@@ -71,7 +71,7 @@ public class TestIntegrationServiceIam extends TestIntegration {
             .displayName(userDisplayName)
             .country(userCountry)
             .dateOfBirth(userDateOfBirth)
-            .uniqueDisplayName(username)
+            .uniqueDisplayName(userName)
             .build();
 
     final ModelUserCreateResponseV3 createUserV3Result =
