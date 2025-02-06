3c2fd17baa3fa09dc33353324562a388ca8b27bf
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2025-02-06 16:13:11 +0700

    feat(api): add login queue service (build.gradle and moduleinfo)

---
+++ module-loginqueue/src/main/java/net/accelbyte/sdk/api/loginqueue/ModuleInfo.java
@@ -0,0 +1,15 @@
+package net.accelbyte.sdk.api.loginqueue;
+
+import net.accelbyte.sdk.core.BaseModuleInfo;
+
+public class ModuleInfo implements BaseModuleInfo {
+  @Override
+  public String getModuleName() {
+    return "module-loginqueue";
+  }
+
+  @Override
+  public String getVersion() {
+    return "0.1.0";
+  }
+}
